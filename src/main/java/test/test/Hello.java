559d5496ea796a9ce957ba8878eb48e0db366e16
Aryan Puchimada <122579751+AryanPuch@users.noreply.github.com>
2026-01-26 21:36:59 -0500

    Function added

--- src/main/java/test/test/Hello.java
+++ src/main/java/test/test/Hello.java
@@ -3,10 +3,14 @@ package test.test;
 
 /**
  *
- * @author Aryan Puchimada
+ * @author Aryan Puchimada, January 2026
  */
 public class Hello {
     public String sayHello() {
         return "Hello World!!!";
+    }
+
+    public double addNumbers(double a, double b) {
+        return a + b;
     }
 }
